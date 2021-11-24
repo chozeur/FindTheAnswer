9ffaba0fd5beccccd2525d86ada74da0b164c325
flcarval <flcarval@student.42.fr>
2021-11-24 02:39:03 +0100

    main up

--- libft/test_main/main.c
+++ libft/test_main/main.c
@@ -6,7 +6,7 @@
 /*   By: flcarval <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 18:33:55 by flcarval          #+#    #+#             */
-/*   Updated: 2021/11/23 19:07:44 by flcarval         ###   ########.fr       */
+/*   Updated: 2021/11/24 02:24:09 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,4 +70,41 @@ int	main(void)
 		printf("ft_tolower : OK\n");
 	else
 		printf("ft_tolower : FAIL\n");
+// strchr
+	char	schr1[256] = "On cherche le caractere [D min] dans cette phrase de trois d";
+	char	Dmin = 'd';
+	if (ft_strchr(schr1, Dmin) == strchr(schr1, Dmin))
+		printf("ft_strchr : OK\n");
+	else
+		printf("ft_strchr : FAIL\n");
+// strrchr
+	if (ft_strrchr(schr1, Dmin) == strrchr(schr1, Dmin))
+		printf("ft_strrchr : OK\n");
+	else
+		printf("ft_strrchr : FAIL\n");
+// strnstr
+	char	look0[256] = "Just look at it";
+	char	tofind[256] = "look";
+
+	if (ft_strnstr(look0, tofind, 42) == strnstr(look0, tofind, 42) && ft_strnstr(look0, tofind, 3) == strnstr(look0, tofind, 3))
+		printf("ft_strnstr : OK\n");
+	else
+		printf("ft_strnstr : FAIL\n");
+// strncmp
+	char	cmp0[256] = "Verifions ft_strncmp";
+	char	cmp1[256] = "Verifions ft_strncmp";
+	char	cmp2[256] = "Verification de ft_strncmp";
+
+	if (ft_strncmp(cmp0, cmp1, 42) == strncmp(cmp0, cmp1, 42) && ft_strncmp(cmp0, cmp1, 5) == strncmp(cmp0, cmp1, 5) && ft_strncmp(cmp0, cmp2, 42) == strncmp(cmp0, cmp2, 42) && ft_strncmp(cmp0, cmp2, 5) == strncmp(cmp0, cmp2, 5))
+		printf("ft_strncmp : OK\n");
+	else
+		printf("ft_strncmp : FAIL\n");
+// strlen
+	char	slen0[256] = "test len";
+	char	slen1[256] = "";
+
+	if (ft_strlen(slen0) == strlen(slen0) && ft_strlen(slen1) == strlen(slen1))
+		printf("ft_strlen : OK\n");
+	else
+		printf("ft_strlen : FAIL\n");
 }
