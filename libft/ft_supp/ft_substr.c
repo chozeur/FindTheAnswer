38e7b95629f6a364efd4c24d3e78c5f24a586540
Florian CARVALHO <flcarval@e2r10p19.clusters.42paris.fr>
2021-11-25 20:06:09 +0100

    ft_supp up

--- libft/ft_supp/ft_substr.c
+++ libft/ft_supp/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: flcarval <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 16:54:42 by flcarval          #+#    #+#             */
-/*   Updated: 2021/11/24 17:13:43 by flcarval         ###   ########.fr       */
+/*   Updated: 2021/11/25 20:05:44 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*res;
-	int	i;
+	size_t	i;
 
 	res = malloc(sizeof(char) * (len + 1));
 	if (res == NULL)
@@ -28,4 +28,18 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	}
 	res[i] = '\0';
 	return (res);
+}
+
+/////////////////////////////////////////////////
+
+#include <stdio.h>
+
+int	main()
+{
+	char	str[256] = "test de ft_substr doit afficher : ft_substr";
+	unsigned int	start = 8;
+	size_t	len = 9;
+	
+	printf("str = %s\tstart = %d\tlen = %ld\tsub = %s\n", str, start, len, ft_substr(str, start, len));
+	return (0);
 }
