9f62de31d6a74f6a9e1f59a8dfbe7d6224042caf
Florian CARVALHO <flcarval@e3r2p18.clusters.42paris.fr>
2021-11-29 15:55:27 +0100

    ft_bonus up

--- libft/ft_bonus/ft_lstclear.c
+++ libft/ft_bonus/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: flcarval <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 14:51:53 by flcarval          #+#    #+#             */
-/*   Updated: 2021/11/29 14:58:47 by flcarval         ###   ########.fr       */
+/*   Updated: 2021/11/29 15:16:08 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 	{
 		j = i;
 		i = i->next;
-		ft_lstdelone(j, (*del)(void *));
+		ft_lstdelone(j, del);
 	}
 }
--- libft/ft_bonus/ft_lstmap.c
+++ libft/ft_bonus/ft_lstmap.c
@@ -6,13 +6,30 @@
 /*   By: flcarval <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 15:06:44 by flcarval          #+#    #+#             */
-/*   Updated: 2021/11/29 15:09:07 by flcarval         ###   ########.fr       */
+/*   Updated: 2021/11/29 15:30:39 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*dedl)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	
+	t_list	*res;
+	t_list	*new;
+
+	if (lst == NULL)
+		return (NULL);
+	res = lst;
+	while (lst)
+	{
+		new = ft_lstnew(f(lst->content));
+		if (new == NULL)
+		{
+			ft_lstclear(&res, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&res, new);
+		lst = lst->next;
+	}
+	return (res);
 }
