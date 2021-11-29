7634825f29fb30b8dc24bb64132a64793e6f3480
Florian CARVALHO <flcarval@e3r2p18.clusters.42paris.fr>
2021-11-29 15:09:13 +0100

    ft_bonus up

--- libft/ft_bonus/ft_lstadd_back.c
+++ libft/ft_bonus/ft_lstadd_back.c
@@ -6,8 +6,25 @@
 /*   By: flcarval <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 10:49:20 by flcarval          #+#    #+#             */
-/*   Updated: 2021/11/29 10:50:33 by flcarval         ###   ########.fr       */
+/*   Updated: 2021/11/29 13:08:19 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../header/libft.h"
 
+void	ft_lstadd_back(t_list **alst, t_list *new)
+{
+	t_list	*i;
+
+	i = *alst;
+	while (i)
+	{
+		if (i->next == NULL)
+		{
+			i->next = new;
+			i->next->next = NULL;
+			break ;
+		}
+		i = i->next;
+	}
+}
