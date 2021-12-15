e888c6b136bfba7f48b83fd5d5395d0f4d534213
Florian CARVALHO <flcarval@e3r3p11.42.fr>
2021-12-15 19:25:26 +0100

    changed ft_printf_p for MacOS

--- ft_printf/srcs/ft_printf_p.c
+++ ft_printf/srcs/ft_printf_p.c
@@ -6,7 +6,7 @@
 /*   By: flcarval <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 17:45:02 by flcarval          #+#    #+#             */
-/*   Updated: 2021/12/15 18:03:58 by flcarval         ###   ########.fr       */
+/*   Updated: 2021/12/15 19:17:43 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	ft_printf_p(void *ptr)
 
 	if (!ptr)
 	{
-		ft_putstr("(nil)");
-		return (5);
+		ft_putstr("0x0");
+		return (3);
 	}
 	ad = (unsigned long int)ptr;
 	ft_putstr("0x");
