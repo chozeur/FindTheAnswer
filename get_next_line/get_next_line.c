dc180563da086493bab413afee729afdb66a5a9a
Florian CARVALHO <flcarval@e2r5p3.clusters.42paris.fr>
2021-12-21 21:12:42 +0100

    gnl in progress

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,10 +6,43 @@
 /*   By: flcarval <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 15:00:41 by flcarval          #+#    #+#             */
-/*   Updated: 2021/12/16 15:01:09 by flcarval         ###   ########.fr       */
+/*   Updated: 2021/12/21 21:12:19 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int	is_nl(char *str)
+{
+	int	i;
 
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	static int	index;
+	char	*res;
+	char	*buff;
+
+	res = ft_strdup("");;
+	if (!res)
+		return (NULL);
+	buff = malloc(BUFFER_SIZE + 1);
+	if (!buff)
+		return (NULL);
+	index = 0;
+	if (fd < 1)
+		return (NULL);
+	read(fd, buff, BUFFER_SIZE);
+	buff[BUFFER_SIZE] = '\0';
+	res = ft_strjoin(res, buff);
+	if (!is_nl(res))
+}
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: flcarval <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 14:54:47 by flcarval          #+#    #+#             */
-/*   Updated: 2021/12/16 15:00:30 by flcarval         ###   ########.fr       */
+/*   Updated: 2021/12/21 20:28:46 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <unistd.h>
 
-
+char	*get_next_line(int fd);
+int	is_nl(char *str);
 
 #endif
