a1d7551722491b9aa7c4fe709f15fe76d37fc1c9
flcarval <flcarval@student.42.fr>
2022-01-07 06:59:02 +0100

    monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -6,7 +6,7 @@
 #    By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/01/04 23:21:28 by flcarval          #+#    #+#              #
-#    Updated: 2022/01/07 01:02:37 by flcarval         ###   ########.fr        #
+#    Updated: 2022/01/07 00:28:06 by flcarval         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -27,63 +27,65 @@
 
 #!/bin/bash
 
+
 NC='\033[0m'
-RED='\033[0;31m'
 PURPLE='\033[0;35m'
 ORANGE='\033[0;33m'
 CYAN='\033[0;36m'
 WHITE='\033[1;37m'
 
 monitoring () {
-	echo -e "${PURPLE}                       _ _           _                _   
+	echo -e "                       _ _           _                _   
 	 _____ ___ ___|_| |_ ___ ___|_|___ ___    ___| |_ 
 	|     | . |   | |  _| . |  _| |   | . |_ |_ -|   |
-	${ORANGE}|_|_|_|___|_|_|_| | |___|_| |_|_|_|_  |_||___|_|_|
+	|_|_|_|___|_|_|_| | |___|_| |_|_|_|_  |_||___|_|_|
 			|__|              |___|           
 	"
+	ARCH=$(uname -p)
+	echo -e "> Architecture   : ${ARCH} (host arch is : x86_64)";
 
-	echo -e -n "${CYAN}> Architecture   : ${NC}";uname -p;
-
-	echo -e -n "${CYAN}> Kernel version : ${NC}";uname -v;
+	echo -e -n "> Kernel version : ";uname -v;
 
 	CPU=$(lscpu | grep "Core(s) per socket" | tr -dc '0-9')
-	echo -e "${CYAN}> Physical cores : ${NC}${CPU}";
+	echo -e "> Physical cores : ${CPU}";
 
-	echo -e -n "${CYAN}> Logical cores  : ${NC}";cat /proc/cpuinfo | grep processor | wc -l;
+	echo -e -n "> Logical cores  : ";cat /proc/cpuinfo | grep processor | wc -l;
 
 	MEM=$(cat /proc/meminfo | grep "MemTotal" | tr -dc '0-9')
 	AVA=$(cat /proc/meminfo | grep "MemAvailable" | tr -dc '0-9')
 	USED=$(($MEM - $AVA))
 	PERCENT=$((($USED * 100) / $MEM))
-	echo -e "${CYAN}> Total memmory  : ${NC}${MEM} Kilo-Bytes ${WHITE}[${PERCENT}% used]";
+	echo -e "> Total memmory  : ${MEM} Kilo-Bytes [${PERCENT}% used]";
 
 	VOL=$(df -l --output=source,size --total | grep total | tr -dc '0-9')
 	PERC=$(df -l --outpu=source,pcent --total | grep total | tr -dc '0-9')
-	echo -e "${CYAN}> Total volume   : ${NC}${VOL} Kilo-Bytes ${WHITE}[${PERC}% used]"
+	echo -e "> Total volume   : ${VOL} Kilo-Bytes [${PERC}% used]"
 
 	CPUP=$(mpstat | grep -A 5 "%idle" | tail -n 1 | awk -F " " '{print 100 -  $ 12}')
-	echo -e "${CYAN}> CPU\t\t : ${WHITE}[${CPUP}% used]"
+	echo -e "> CPU\t\t : [${CPUP}% used]"
 
-	echo -e -n "${CYAN}> Latest reboot  : ${NC}";who -b | cut -c 25-;
+	echo -e -n "> Latest reboot  : ";who -b | cut -c 25-;
 
 	if [ $(lsblk -o type | grep "lvm" | wc -l) -ge 1 ]; then
 		STATUS=ENABLE
 	else
 		STATUS=DISABLE
 	fi
-	echo -e "${CYAN}> LVM status     : ${NC}${STATUS}"
+	echo -e "> LVM status     : ${STATUS}"
 
 	ACTIVE=$(ps -eaho user | sort -u | wc -l)
-	echo -e "${CYAN}> Active users   : ${NC}${ACTIVE} ${WHITE}(root is counted)"
+	echo -e "> Active users   : ${ACTIVE} (root is counted)"
 
-	IPV4=$(ifconfig | grep --max-count=1 "inet" | cut -b 14-23)
-	echo -e "${CYAN}> ipv4 address   : ${NC}${IPV4}"
+	IPV4=$(ip a | grep ether -1 | grep inet | cut -b 10-18)
+	echo -e "> ipv4 address   : ${IPV4}"
 
-	MAC=$(ifconfig | grep --max-count=1 "ether" | cut -b 15-31)
-	echo -e "${CYAN}> MAC address    : ${NC}${MAC}"
+	MAC=$(ip a | grep ether | cut -b 16-32)
+	echo -e "> MAC address    : ${MAC}"
 
 	SC=$(($(cat /var/log/sudo/commands.log | wc -l) / 2))
-	echo -e "${CYAN}> sudo commands  : ${NC}${SC}"
+	echo -e "> sudo commands  : ${SC}"
 }
 
-wall monitoring
+rm -f /var/log/monitoring/monitoring.sh
+monitoring > /var/log/monitoring/monitoring.log
+wall /var/log/monitoring/monitoring.log 
