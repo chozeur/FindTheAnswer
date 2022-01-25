1254145cf0f878bf9366b7af3df045927e1c54ed
flcarval <flcarval@student.42.fr>
2022-01-26 00:04:05 +0100

    push_swap in progress

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,8 +6,23 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 00:05:46 by flcarval          #+#    #+#             */
-/*   Updated: 2022/01/18 21:22:01 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/01/26 00:03:04 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+int	main(int ac, char **av)
+{
+	t_number	*lst_a;
+	t_number	*lst_b;
+	t_number	**alst_a;
+	t_number	**alst_b;
+
+	if (ac == 1)
+	{
+		ft_putstr("You must enter at least 1 integer.\n");
+		return (-1);
+	}
+
+}
