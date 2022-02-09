b6f121523f0695fa3f3084034a015f7630385095
Florian CARVALHO <flcarval@e1r5p2.42.fr>
2022-02-09 17:06:25 +0100

    sortsack_big works (100 -> 282688 lol)

--- push_swap/src/algo/sortstack_big.c
+++ push_swap/src/algo/sortstack_big.c
@@ -6,7 +6,7 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 01:57:24 by flcarval          #+#    #+#             */
-/*   Updated: 2022/02/08 16:41:12 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/02/09 17:05:10 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,7 @@ void	sortstack_big(t_number **alst_a)
 {
 	t_number	*lst_b;
 	t_number	**alst_b;
-		int		*sorted;
 
-	sorted = malloc(sizeof(int) * (ft_lstsize(*alst_a) + 1));
-	if (!sorted)
-		return ;
-	sorted[0] = 1;
 	if (is_sorted(alst_a))
 	{
 		ft_putstr("Stack is already sorted.\n");
@@ -31,52 +26,50 @@ void	sortstack_big(t_number **alst_a)
 	alst_b = &lst_b;
 	while (!is_sorted(alst_a))
 	{
-		part(ft_lstsize(*alst_a), alst_a, alst_b, sorted);
-		sorted[0]++;
+		part(ft_lstsize(*alst_a), alst_a, alst_b);
+		rotate(alst_a, "ra\n");
 	}
 }
 
-void	part(int size, t_number **alst_a, t_number **alst_b, int *sorted)
+void	part(int size, t_number **alst_a, t_number **alst_b)
 {
 		int		piv_num;
 		int		i;
-		int		low;
 
 	piv_num = (*alst_a)->num;
-	low = lowest(alst_a);
-	//if (piv_num == low)
-	//	reverse_rotate(alst_a, "rra\n");
 	push(alst_a, alst_b, "pb\n");
 	i = 0;
 	while (i < size - 1)
 	{
-		if (isplaced(piv_num, sorted))
-			rotate(alst_a, "rra\n");
-		else if ((*alst_a)->num < piv_num)
+		if ((*alst_a)->num > piv_num)
 			push(alst_a, alst_b, "pb\n");
 		else
-			rotate(alst_a, "rra\n");
+		{
+			if (next_gt_index(alst_a) < (ft_lstsize(*alst_a) / 2))
+				rotate(alst_a, "ra\n");
+			else
+				reverse_rotate(alst_a, "rra\n");
+		}
 		i++;
 	}
-	reverse_rotate(alst_b, "rrb\n");
 	emptystack(alst_a, alst_b);
-	sorted[sorted[0]] = piv_num;
 }
 
-int	isplaced(int num, int *sorted)
+int	next_gt_index(t_number **alst_a)
 {
-	int	i;
+	t_number	*lst;
+		int		i;
 
+	lst = *alst_a;
 	i = 1;
-	if (sorted[0] == 1)
-		return (0);
-	while (i < sorted[0])
+	while (lst && lst->num < (*alst_a)->num)
 	{
-		if (sorted[i] == num)
-			return (1);
+		lst = lst->next;
 		i++;
 	}
-	return (0);
+	if (!lst)
+		return (0);
+	return (i);
 }
 
 void	emptystack(t_number **alst_a, t_number **alst_b)
