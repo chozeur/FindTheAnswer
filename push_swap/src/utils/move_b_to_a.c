c82fa683c28312586c873e8dfd6e49a6c38b07ed
flcarval <flcarval@student.42.fr>
2022-02-17 15:48:03 +0100

    new version : 100 -> ~1400

--- push_swap/src/utils/move_b_to_a.c
+++ push_swap/src/utils/move_b_to_a.c
@@ -6,7 +6,7 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 15:12:01 by flcarval          #+#    #+#             */
-/*   Updated: 2022/02/17 15:41:21 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/02/17 15:47:26 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,12 @@ void	move_b_to_a(t_number **alst_a, t_number **alst_b, int init_size)
 
 	while (ft_lstsize(*alst_b))
 	{
-		if (ft_lstsize(*alst_b) <= init_size / 2)
+		if ((*alst_b)->next && (*alst_b)->next->num == highest(alst_b))
+		{
+			swap(alst_b, "sb\n");
+			push(alst_b, alst_a, "pa\n");
+		}
+		else if (ft_lstsize(*alst_b) <= init_size / 2)
 		{
 			while ((*alst_b)->num != highest(alst_b))
 			{
@@ -27,22 +32,20 @@ void	move_b_to_a(t_number **alst_a, t_number **alst_b, int init_size)
 			}
 			push(alst_b, alst_a, "pa\n");
 		}
-		else if ((*alst_b)->next && (*alst_b)->next->num == highest(alst_b))
+		else
 		{
-			swap(alst_b, "sb\n");
+			i = 0;
+			while ((*alst_b)->num != highest(alst_b))
+			{
+				rotate(alst_b, "rb\n");
+				i++;
+			}
 			push(alst_b, alst_a, "pa\n");
-		}
-		i = 0;
-		while ((*alst_b)->num != highest(alst_b))
-		{
-			rotate(alst_b, "rb\n");
-			i++;
-		}
-		push(alst_b, alst_a, "pa\n");
-		while (i)
-		{
-			reverse_rotate(alst_b, "rrb\n");
-			i--;
+			while (i)
+			{
+				reverse_rotate(alst_b, "rrb\n");
+				i--;
+			}
 		}
 	}
 }
