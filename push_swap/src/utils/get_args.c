275ab2ac764ea8c91835520637f2b62a5bc438eb
Florian CARVALHO <flcarval@e1r4p6.42.fr>
2022-03-09 17:53:46 +0100

    secure wip

--- push_swap/src/utils/get_args.c
+++ push_swap/src/utils/get_args.c
@@ -6,11 +6,13 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 04:53:43 by flcarval          #+#    #+#             */
-/*   Updated: 2022/03/09 03:42:05 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/03/09 17:32:06 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/push_swap.h"
+
+static int	is_wss(char *str);
 
 t_number	*get_args(int ac, char **av)
 {
@@ -23,7 +25,11 @@ t_number	*get_args(int ac, char **av)
 		return (NULL);
 	}
 	if (ac == 2)
+	{
+		if (av[1][0] == '\0' || is_wss(av[1]))
+			return (NULL);
 		args = ft_split(av[1], ' ');
+	}
 	else
 		args = &av[1];
 	if (!is_valid_input(args, ac))
@@ -35,4 +41,15 @@ t_number	*get_args(int ac, char **av)
 		return (NULL);
 	}
 	return (lst);
+}
+
+static int	is_wss(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		if (str[i++] != ' ')
+			return (0);
+	return (1);
 }
--- push_swap/src/utils/is_valid_input.c
+++ push_swap/src/utils/is_valid_input.c
@@ -6,7 +6,7 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/21 19:28:45 by flcarval          #+#    #+#             */
-/*   Updated: 2022/03/09 03:38:32 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/03/09 17:52:16 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 static int	error(void);
 static int	isonly_num(char *str);
-static int	isdup(char **inputs, int ac);
-static int	is_al(int num, int *tab, int ac);
 
 int	is_valid_input(char **inputs, int ac)
 {
 	int	i;
 
+	(void)ac;
 	i = 0;
 	while (inputs[i])
 	{
@@ -32,7 +31,6 @@ int	is_valid_input(char **inputs, int ac)
 			return (error());
 		i++;
 	}
-	isdup(inputs, ac);
 	return (1);
 }
 
@@ -53,43 +51,8 @@ static int	isonly_num(char *str)
 	return (1);
 }
 
-static int	isdup(char **inputs, int ac)
-{
-	int	*tab;
-	int	i;
-
-	tab = malloc(sizeof(int) * ac);
-	if (!tab)
-		return (-1);
-	i = 0;
-	while (i < ac)
-	{
-		if (is_al(ft_atol(inputs[i]), tab, ac))
-			return (error());
-		else
-			tab[i] = ft_atol(inputs[i]);
-		i++;
-	}
-	free(tab);
-	return (1);
-}
-
-static int	is_al(int num, int *tab, int ac)
-{
-	int	i;
-
-	i = 0;
-	while (i < ac)
-	{
-		if (num == tab[i])
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
 static int	error(void)
 {
 	ft_putstr("Please enter (single) integers only.\n");
-	return (-1);
+	return (0);
 }
