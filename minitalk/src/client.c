cbdd288b938daeec0662520e7f6898f956beda31
ChoZeur <flcarval@student.42.fr>
2022-03-16 20:55:46 +0100

    MAKEFILE OK

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -6,7 +6,7 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 23:42:52 by flcarval          #+#    #+#             */
-/*   Updated: 2022/03/03 01:21:26 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/03/16 18:47:41 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,11 @@ int	main(int ac, char **av)
 	pid_t	pid;
 	char	*msg;
 
+	(void)ac;
 	msg = malloc(sizeof(char) * (ft_strlen(av[2]) + 1));
+	pid = (pid_t)ft_atoi(av[1]);
 	if (!pid && !msg)
 		return (-1);
-	pid = (pid_t)ft_atoi(av[1]);
 	ft_strlcpy(msg, av[2], ft_strlen(av[2]) + 1);
 	kill(pid, SIGUSR1);
 	return (0);
