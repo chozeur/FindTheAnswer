0b0ab267833f2b3702a5ac3da16a4de985dfc8d1
ChoZeur <flcarval@student.42.fr>
2022-03-27 19:39:05 +0200

    sl wip

---
+++ so_long/src/so_long.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/27 06:22:22 by flcarval          #+#    #+#             */
+/*   Updated: 2022/03/27 06:31:53 by flcarval         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long.h"
+
+int main(void)
+{
+	void	*mlx_ptr;
+	void	*window;
+
+	mlx_ptr = mlx_init();
+	window = mlx_new_window(mlx_ptr, 500, 500, "Hello world");
+	return (0);
+}
