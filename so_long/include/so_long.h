92c4e244d12d9ed2e19a0d9d88a25aad25796610
ChoZeur <flcarval@student.42.fr>
2022-04-02 02:45:29 +0200

    added Lib42

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -6,17 +6,26 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 06:20:28 by flcarval          #+#    #+#             */
-/*   Updated: 2022/03/30 15:38:37 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/04/02 02:43:55 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include "../minilibx-linux/mlx.h"
-#include "../libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <X11/keysym.h>
+// # include <mlx.h>
+# include "../minilibx-linux/mlx.h"
+# include "../Lib42/include/lib42.h"
 
+# define MLX_ERROR 1
+# define RED_PIXEL 0xFF0000
+
+typedef struct s_data{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_data;
 
 #endif
--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -6,21 +6,43 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/27 06:22:22 by flcarval          #+#    #+#             */
-/*   Updated: 2022/03/30 16:14:58 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/04/01 00:22:48 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-int main(void)
+int	handle_no_event(void *data)
 {
-	void	*mlx_ptr;
-	void	*window;
+	(void)data;
+	return (0);
+}
 
-	mlx_ptr = mlx_init();
-	window = mlx_new_window(mlx_ptr, 500, 500, "Hello world");
-	(void)window;
-	ft_putstr_fd("Hello World !\n", 1);
-	while (1)
+int	handle_input(int keysym, t_data *data)
+{
+	if (keysym == XK_Escape)
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+	
+	return (0);
+}
+
+int	main(void)
+{
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	if (!data.mlx_ptr)
+		return (MLX_ERROR);
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 960, 540, "so_long");
+	if (!data.win_ptr)
+	{
+		free(data.mlx_ptr);
+		return (MLX_ERROR);
+	}
+	mlx_loop_hook(data.mlx_ptr, &handle_no_event, &data);
+	mlx_key_hook(data.win_ptr, &handle_input, &data);
+	mlx_loop(data.mlx_ptr);
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
 	return (0);
 }
