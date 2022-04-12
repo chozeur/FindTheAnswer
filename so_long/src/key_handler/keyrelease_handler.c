b3d626b5b552b5d5c0fe2000fe7b4426ce6e35da
ChoZeur <flcarval@student.42.fr>
2022-04-12 11:51:32 +0200

    +

--- so_long/src/key_handler/keyrelease_handler.c
+++ so_long/src/key_handler/keyrelease_handler.c
@@ -6,7 +6,7 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 23:52:36 by flcarval          #+#    #+#             */
-/*   Updated: 2022/04/04 23:52:46 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/04/05 18:07:30 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	keyrelease_handler(int keysym, t_data *data)
 	if (keysym == XK_Escape)
 	{
 		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		//mlx_destroy_display(data->mlx_ptr);
 		data->win_ptr = NULL;
 	}
 	return (0);
