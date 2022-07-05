da538f731a3108e505362c295acee7ea0ce7fcb6
Florian CARVALHO <flcarval@student.42.fr>
2022-07-05 18:44:07 +0200

    created header & main files

---
+++ philosophers/include/philosophers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 18:34:05 by flcarval          #+#    #+#             */
+/*   Updated: 2022/07/05 18:40:09 by flcarval         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+#endif
