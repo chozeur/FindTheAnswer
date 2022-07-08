c3a29e60cfc7099bf0a49bbe6010badc4635daad
ChoZeur <flcarval@student.42.fr>
2022-07-08 23:32:06 +0200

    [philo] forks & table init is all good

--- philosophers/src/main.c
+++ philosophers/src/main.c
@@ -6,13 +6,13 @@
 /*   By: flcarval <flcarval@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 18:42:59 by flcarval          #+#    #+#             */
-/*   Updated: 2022/07/08 19:27:08 by flcarval         ###   ########.fr       */
+/*   Updated: 2022/07/08 23:23:03 by flcarval         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-static int test(t_philo *tab, int n, t_args args);
+static int test(t_philo *tab, int n, t_args args, t_data *data);
 
 int	main(int ac, char **av)
 {
@@ -22,7 +22,7 @@ int	main(int ac, char **av)
 		exit(EXIT_FAILURE);
 	if (init_all(&data))
 		return (1);
-	test(data.table, data.args.pnum, data.args);
+	test(data.table, data.args.pnum, data.args, &data);
 	return (free_philo(&data));
 }
 
@@ -30,7 +30,7 @@ int	main(int ac, char **av)
 
 // TEST DE LA RECUP DES ARG ET DE L'INIT DE LA TABLE ET DES FOURCHETTES
 //
-static int test(t_philo *tab, int n, t_args args)
+static int test(t_philo *tab, int n, t_args args, t_data *data)
 {
 	int i = 0;
 
@@ -40,7 +40,7 @@ static int test(t_philo *tab, int n, t_args args)
 	while (i < n)
 	{
 		printf("philo n°%d\tprev = %ld\tlun = %d\tstate = %d\tleft_fork = %p\tright_fork = %p\n", \
-			tab[i].num, tab[i].prev_lunch, tab[i].lunches, tab[i].state, &tab[i].left_fork, &tab[i].right_fork);
+			tab[i].num, tab[i].prev_lunch, tab[i].lunches, tab[i].state, &data->forks[tab[i].left_fork], &data->forks[tab[i].right_fork]);
 		i++;
 	}
 	return (0);
